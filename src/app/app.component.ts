e87be202137a365e8c9d1aea456a4d0ea7b1eb88
Fida Muntaseer <fida.muntaseer.1989@gmail.com>
2024-02-13 19:19:23 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Ng2ImgMaxService } from 'ng2-img-max';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'angular-image-compress';
+  imageUploadForm: FormGroup;
+  sourceImage: string;
+  resultImage: string;
+  sourceSize: string;
+  resultSize: string;
+
+  constructor(private formBuilder: FormBuilder, private ng2ImgMax: Ng2ImgMaxService) {
+    this.imageUploadForm = this.formBuilder.group({
+      fileInput: ''
+    });
+    this.sourceImage = 'https://angular.io/assets/images/logos/angular/angular.svg';
+    this.resultImage = 'https://angular.io/assets/images/logos/angular/angular.svg';
+    this.sourceSize = '';
+    this.resultSize = '';
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.sourceImage = event.target.result;
+        this.sourceSize = ((file.size) / 1024) / 1024 + " MB";
+      }
+      reader.readAsDataURL(file);
+      const maxSizeInMB = 1;
+      this.compressImage(file, maxSizeInMB);
+    }
+  }
+
+  compressImage(file: File, maxSizeInMB: number) {
+    this.ng2ImgMax.compressImage(file, maxSizeInMB)
+      .subscribe(compressedImage => {
+        var reader = new FileReader();
+        reader.onload = (event: any) => {
+          this.resultImage = event.target.result;
+          this.resultSize = ((compressedImage.size) / 1024) / 1024 + " MB";
+        }
+        reader.readAsDataURL(compressedImage);
+      }, error => {
+        console.log(error.reason);
+      });
+  }
 }
